0991c9da8f424dc121356ef8fae1b2108c97c3e7
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-05-23 14:20:19 +0100

    Added gated components

---
+++ dapp/components/Governance.tsx
@@ -0,0 +1,8 @@
+export default function Governance() {
+    return (
+      <>
+        <h2 className="text-4xl mb-4">Governance</h2>
+      </>
+    );
+  }
+  
--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -6,13 +6,56 @@ import Layout from "../components/Layout";
 import Prose from "../components/Prose";
 import Minting from "../components/Minting";
 import Staking from "../components/Staking";
-import Faq from "../components/Faq";
-import Roadmap from "../components/Roadmap";
+import Governance from "../components/Governance";
 import Team from "../components/Team";
 import projectConfig from "../config/projectConfig";
 import topImage from "../public/assets/1920x600.png";
+import { useEffect, useState } from "react";
+
+import ABI from "../config/abi.json";
+import rpcConfig from "../config/rpcConfig";
+import { useEthereumProvider } from "../hooks/useEthereumProvider";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
 
 const Home: NextPage = () => {
+  const { account, active, chainId } = useWeb3React();
+  const { ethereumProvider } = useEthereumProvider();
+
+  // User's balance of NftVestingDao NFTs
+  const [nftBalance, setNftBalance] = useState(0);
+
+  // Reads the balance of the user's NftVestingDao NFTs and sets the `nftBalance` state variable
+  useEffect(() => {
+    async function getUserNFTBalance() {
+      if (account && ethereumProvider) {
+        try {
+          const web3Provider = new ethers.providers.JsonRpcProvider(
+            rpcConfig(process.env.NEXT_PUBLIC_INFURA_KEY)
+          );
+          const contract = new ethers.Contract(
+            projectConfig.contractAddress,
+            ABI,
+            web3Provider
+          );
+
+          const balance = await contract.balanceOf(account);
+          // setNftBalance(balance);
+          setNftBalance(parseInt(balance.toString()));
+          console.log(`User's NFT balance: ${balance}`);
+          console.log(`User's Account: ${account}`);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    getUserNFTBalance();
+
+    // cleanup
+    return () => setNftBalance(0);
+  }, [account, ethereumProvider]);
+
   return (
     <Layout>
       <Head>
@@ -42,25 +85,24 @@ const Home: NextPage = () => {
         </Prose>
       </div>
 
+      {nftBalance && nftBalance > 10 ? (
+        <>
+          <div className="py-8">
+            <Prose>
+              <Staking />
+            </Prose>
+          </div>
+          <div className="py-8">
+            <Prose>
+              <Governance />
+            </Prose>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+
       <div className="bg-gray-800 py-8">
-        <Prose>
-          <Staking />
-        </Prose>
-      </div>
-
-      <div className="py-8">
-        <Prose>
-          <Faq />
-        </Prose>
-      </div>
-
-      <div className="bg-gray-800 py-8">
-        <Prose>
-          <Roadmap />
-        </Prose>
-      </div>
-
-      <div className="py-8">
         <Prose>
           <Team />
         </Prose>
